41da0e75dd86846cdb7bb071a7b0389164cd2c20
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-07-11 17:09:17 +0100

    Basic format for framework

--- src/CC1P1PiAnalysis.cpp
+++ src/CC1P1PiAnalysis.cpp
@@ -1,2 +1,46 @@
-#ifndef NUMUCC1P1PIANALYSIS_CPP
-#define NUMUCC1P1PIANALYSIS_CPP
+#include "CC1P1PiAnalysis.h"
+
+// ************** Forward Declared Headers ************** //
+
+
+
+// ************** Other Headers ************** //
+
+
+
+ECLARE_TOOL_FACTORY( CC1P1PiAnalysis );
+
+using namespace Minerva;
+
+CC1P1PiAnalysis::CC1P1PiAnalysis( const std::string& type, const std::string& name, const IInterface* parent ) : MinervaAnalysisTool( type, name, parent ){
+    
+    // ************** Mandatory Code: ************** //
+    declareInterface<IInteractionHypothesis>(this);
+    m_anaSignature = "numuCC1P1PiAnalysis";
+    m_hypMeths.push_back( "numuCC1P1Pi" );
+    declareProperty("HypothesisMethods", m_hypMeths);
+    // ********************************************* //
+
+    
+    
+}
+
+StatusCode CC1P1PiAnalysis::initialize(){
+    
+}
+
+StatusCode CC1P1PiAnalysis::finalize(){
+    
+}
+
+StatusCode CC1P1PiAnalysis::reconstructEvent( Minerva::PhysicsEvent* event, Minerva::GenMinInteraction* truth = NULL ) const{
+    
+}
+
+StatusCode CC1P1PiAnalysis::interpretEvent( const Minerva::PhysicsEvent* event, const Minerva::GenMinInteraction* truth, std::vector<Minerva::NeutrinoInt*>& nuInts ) const{
+    
+}
+
+StatusCode CC1P1PiAnalysis::tagTruth( Minerva::GenMinInteraction* truth ) const{
+    
+}
--- src/CC1P1PiAnalysis.h
+++ src/CC1P1PiAnalysis.h
@@ -1,3 +1,27 @@
 #ifndef CC1P1PIANALYSIS_H
-#define CC1P1PIANALYSIS_H
+#define CC1P1PIANALYSIS_H 1
 
+#include "AnaUtils/MinervaAnalysisTool.h"
+
+// ************** Forward Declarations ************** //
+
+
+
+class CC1P1PiAnalysis : public MinervaAnalysisTool{
+
+public:
+    
+    CC1P1PiAnalysis( const std::string& type, const std::string& name, const IInterface* parent );
+    ~CC1P1PiAnalysis(){};
+    
+    StatusCode initialize();
+    
+    StatusCode finalize();
+    
+    StatusCode reconstructEvent( Minerva::PhysicsEvent* event, Minerva::GenMinInteraction* truth = NULL ) const;
+    
+    StatusCode interpretEvent( const Minerva::PhysicsEvent* event, const Minerva::GenMinInteraction* truth, std::vector<Minerva::NeutrinoInt*>& nuInts ) const;
+    
+    StatusCode tagTruth( Minerva::GenMinInteraction* truth ) const;
+    
+};
