9611fdcc1874a497165e60ee5ae66a05b677ad47
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-07-11 17:27:18 +0100

    Basic format for framework

--- src/CC1P1PiAnalysis.cpp
+++ src/CC1P1PiAnalysis.cpp
@@ -12,7 +12,7 @@ ECLARE_TOOL_FACTORY( CC1P1PiAnalysis );
 
 using namespace Minerva;
 
-CC1P1PiAnalysis::CC1P1PiAnalysis( const std::string& type, const std::string& name, const IInterface* parent ) : MinervaAnalysisTool( type, name, parent ){
+CC1P1PiAnalysis::CC1P1PiAnalysis( const std::string& type, const std::string& name, const IInterface* parent ) : MinervaAnalysisTool( type, name, parent ) {
     
     // ************** Mandatory Code: ************** //
     declareInterface<IInteractionHypothesis>(this);
@@ -27,20 +27,55 @@ CC1P1PiAnalysis::CC1P1PiAnalysis( const std::string& type, const std::string& na
 
 StatusCode CC1P1PiAnalysis::initialize(){
     
+    StatusCode sc = this->MinervaAnalysisTool::initialize();
+    
+    if( sc.isFailure() ){
+        return Error( "Failed to finalize!", sc );
+    }
+    
+    
+    return sc;
 }
 
 StatusCode CC1P1PiAnalysis::finalize(){
+    StatusCode sc = this->MinervaAnalysisTool::finalize();
     
+    if( sc.isFailure() ){
+        return Error( "Failed to finalize!", sc );
+    }
+    
+    return sc;
 }
 
 StatusCode CC1P1PiAnalysis::reconstructEvent( Minerva::PhysicsEvent* event, Minerva::GenMinInteraction* truth = NULL ) const{
+    
+    debug() << "CC1P1PiAnalysis::reconstructEvent : Called." << endmsg;
+    
+    
+    
+    markEvent( event );
+    // Now interpret the event and add NeutrinoInts
+    std::vector<Minerva::NeutrinoInt*> nuInts;
+    interpretEvent( event, truth, nuInts );
+    
+    // Add the newly create NeutrinoInts to this PhysicsEvent
+    StatusCode sc = addInteractionHyp( event, nuInts );
+    
+    return sc;
     
 }
 
 StatusCode CC1P1PiAnalysis::interpretEvent( const Minerva::PhysicsEvent* event, const Minerva::GenMinInteraction* truth, std::vector<Minerva::NeutrinoInt*>& nuInts ) const{
     
+    
+    
+    debug() << "Exit interpretEvent()" << endmsg;
+    
+    return StatusCode::SUCCESS;
 }
 
 StatusCode CC1P1PiAnalysis::tagTruth( Minerva::GenMinInteraction* truth ) const{
+    truth->setIntData("should_be_accepted", 1);
     
+    return StatusCode::SUCCESS;
 }
