0b7addc263656da6d7b5ef4e3e85dc77ed176b2c
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-08-03 18:18:06 +0100

    Bug fixing Transverse code

--- src/CC1P1PiAnalysis.cpp
+++ src/CC1P1PiAnalysis.cpp
@@ -1352,10 +1352,9 @@ TVector3 * CC1P1PiAnalysis::GetTransverseVars(double vtx[], const TVector3 *& mu
     const TVector3 * neutrino_dir = new TVector3(nudir->X(),nudir->Y(), nudir->Z());
     
     const TVector3 * mupT = GetVecT(neutrino_dir, mumom);
-    
-    /*TVector3 * prpT = GetVecT(nudir, prmom);
-    TVector3 * pipT = GetVecT(nudir, pimom);
-    
+    const TVector3 * prpT = GetVecT(neutrino_dir, prmom);
+    const TVector3 * pipT = GetVecT(neutrino_dir, pimom);
+    /*
     TVector3 * deltapt;
     SetDPT(deltapt, mupT, prpT, pipT);
     
