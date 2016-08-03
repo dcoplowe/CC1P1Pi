9b32113d3b2304a57198e3263d38c6fd70d70bd2
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-08-03 18:12:09 +0100

    Bug fixing Transverse code

--- src/CC1P1PiAnalysis.cpp
+++ src/CC1P1PiAnalysis.cpp
@@ -1337,7 +1337,7 @@ void CC1P1PiAnalysis::SetGlobal4Vec(std::string name, Gaudi::LorentzVector vec, 
 //Transverse variables:
 //If is_truth the vtx becomes the true neutrino direction.
 
-TVector3 * CC1P1PiAnalysis::GetTransverseVars(double vtx[3], const TVector3 *& mumom, const TVector3 *& prmom, const TVector3 *& pimom, double &dpTT, double &dpTMag, double &dalphaT, double &dphiT, bool is_truth) const
+TVector3 * CC1P1PiAnalysis::GetTransverseVars(double vtx[], const TVector3 *& mumom, const TVector3 *& prmom, const TVector3 *& pimom, double &dpTT, double &dpTMag, double &dalphaT, double &dphiT, bool is_truth) const
 {
     TVector3 * nudir = new TVector3();
     
