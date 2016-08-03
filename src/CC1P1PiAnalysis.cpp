3aef592aba35aa2bef0e877d9ff94c64f962e461
dcoplowe <david.coplowe@physics.ox.ac.uk>
2016-08-03 18:50:23 +0100

    Bug fixing Transverse code

--- src/CC1P1PiAnalysis.cpp
+++ src/CC1P1PiAnalysis.cpp
@@ -970,9 +970,9 @@ void CC1P1PiAnalysis::FillCommonBranches(const Minerva::GenMinInteraction *truth
     
     TVector3 * dpT_3mom = GetTransverseVars(vertex, mu_p, pr_p, pi_p, dpTT, dpTMag, dalphaT, dphiT);
     std::vector<double> vec_dpT_3mom;
-    dpT_3mom.push_back(dpT_3mom->X());
-    dpT_3mom.push_back(dpT_3mom->Y());
-    dpT_3mom.push_back(dpT_3mom->Z());
+    vec_dpT_3mom.push_back(dpT_3mom->X());
+    vec_dpT_3mom.push_back(dpT_3mom->Y());
+    vec_dpT_3mom.push_back(dpT_3mom->Z());
     
     cc1p1piHyp->setDoubleData("dpTT", dpTT);
     cc1p1piHyp->setDoubleData("dpT", dpTMag);
