07d5213c4e0dc4b212dfed0cdd2cd9a05ea4f7fc
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-03-22 19:33:39 +0000

    New Post analysis reader

--- app/RunPostProcesses.cxx
+++ app/RunPostProcesses.cxx
@@ -2,7 +2,7 @@
 #define _RUNPOSTPROCESSES_H
 
 // #include <AnalysisReader.h>//This includes the classes to read the tree or copy a tree
-#include <src/TransverseTools.h>
+#include <TransverseTools.h>
 
 #include <cassert>
 #include <iostream>
